49323ea551c1357b09396fc057d910645e933f3c
Sai C <saicharan.k.systems@gmail.com>
2026-01-04 20:40:33 +0530

    Create LC_680_valid_palindrome_2.cpp

---
+++ patterns/two_pointers/LC_680_valid_palindrome_2.cpp
@@ -0,0 +1,30 @@
+//TC - O(n)
+//SC - O(1)
+//technique - using a helper func to check palindrome seperately
+
+class Solution {
+public:
+    bool isPalindromeRange(string &s, int left, int right){
+        while(left<right){
+            if(s[left]!=s[right]) return false;
+            left++;
+            right--;
+
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+        int l=0;
+        int r=s.length() -1;
+        while(l<r){
+            if(s[l]==s[r]){
+                l++;
+                r--;
+            } else {
+                return isPalindromeRange(s, l+1, r) || isPalindromeRange(s, l, r-1);
+            }
+            
+        }
+        return true;
+    }
+};
