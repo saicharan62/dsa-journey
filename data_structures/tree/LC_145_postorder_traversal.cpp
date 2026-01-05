9931ace812493e35cbb70e9facdd78b00b2309af
Sai C <saicharan.k.systems@gmail.com>
2026-01-05 21:40:02 +0530

    Create LC_145_postorder_traversal.cpp

---
+++ data_structures/tree/LC_145_postorder_traversal.cpp
@@ -0,0 +1,33 @@
+//TC - O(n)
+//SC - O(n)
+//technique - first traverse for left, then traverse for right, and then print root value.
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void postOrder(TreeNode* root, vector<int>& arr){
+        if(root == nullptr) return;
+
+        
+        postOrder(root -> left, arr);
+        
+        postOrder(root -> right, arr);
+        arr.push_back(root -> val);
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> arr;
+
+        postOrder(root, arr);
+        return arr;
+    }
+};
