b2647338c60f0283f705dd5d5b30b60d5a78bbd7
Sai C <saicharan.k.systems@gmail.com>
2026-01-06 21:39:21 +0530

    Update LC_144_bt_preorder_traversal.cpp

--- data_structures/tree/LC_144_bt_preorder_traversal.cpp
+++ data_structures/tree/LC_144_bt_preorder_traversal.cpp
@@ -2,17 +2,7 @@
 //SC - O(n)
 //technique - first insert root value, then traverse for left, then traverse for right.
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
     void preOrder(TreeNode* root, vector<int>& arr){
@@ -28,5 +18,42 @@ public:
 
         preOrder(root, arr);
         return arr;
+    }
+};
+
+//technique - we use iterative technique, as in use stack to avoid stack overflow in 1st methos, becuase that is out of our control but this method solves that issue.
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    vector<int> preorderTraversal(TreeNode* root){
+        
+        vector<int> ans;
+        if(root == nullptr) return ans;
+
+        stack<TreeNode*> st;
+        st.push(root);
+
+        while(!st.empty()){
+            root = st.top();
+            st.pop();
+
+            ans.push_back(root->val);
+
+            if(root->right) st.push(root->right);
+            if(root->left) st.push(root->left);
+        }
+        return ans;
     }
 };
