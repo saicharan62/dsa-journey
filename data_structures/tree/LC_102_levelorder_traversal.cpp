9ece664535a176793640ead49829c5692cf51a29
Sai C <saicharan.k.systems@gmail.com>
2026-01-06 12:59:09 +0530

    Create LC_102_levelorder_traversal.cpp

---
+++ data_structures/tree/LC_102_levelorder_traversal.cpp
@@ -0,0 +1,30 @@
+//TC - O(n)
+//SC - O(n)
+//technique - use queue for node fetching and a 2D array to store the values level wise.
+
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(root==NULL) return ans;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty()){
+            int size = q.size();
+            vector<int> level;
+            for(int i=0; i<size; i++){
+                TreeNode *node = q.front();
+                q.pop();
+                if(node->left != NULL) q.push(node->left);
+                if(node->right != NULL) q.push(node->right);
+                level.push_back(node->val);
+            }
+            ans.push_back(level);
+            
+        }
+        return ans;
+    }
+};
