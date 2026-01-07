efb3659ae01e12f7a99f8a8f12f186568ec3c1e5
Sai C <saicharan.k.systems@gmail.com>
2026-01-07 19:39:25 +0530

    Update LC_145_postorder_traversal.cpp

--- data_structures/tree/LC_145_postorder_traversal.cpp
+++ data_structures/tree/LC_145_postorder_traversal.cpp
@@ -2,17 +2,6 @@
 //SC - O(n)
 //technique - first traverse for left, then traverse for right, and then print root value.
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     void postOrder(TreeNode* root, vector<int>& arr){
@@ -30,4 +19,39 @@ public:
         postOrder(root, arr);
         return arr;
     }
+};
+
+//TC - O(n)
+//SC - O(n*h)
+//technique - we use 2 stacks, one for traversals, other for final post order saving and an array to fill with values and return finally.
+
+class Solution {
+    public:
+        vector<int> postorderTraversal(TreeNode* root){
+            
+            vector<int> postorder;
+            if(root == NULL) return postorder;
+
+           
+            stack<TreeNode*> st1, st2;
+            st1.push(root);
+            while(!st1.empty()){
+                root = st1.top();
+                st1.pop();
+                st2.push(root);
+
+                if(root->left != NULL){
+                    st1.push(root->left);
+                }
+                if(root->right != NULL){
+                    st1.push(root->right);
+                }
+
+            }
+            while(!st2.empty()){
+                postorder.push_back(st2.top()->val);
+                st2.pop();
+            }
+            return postorder;
+        }
 };
