9b9d25f78d99906a2860dd31efd1516847d1ce47
Sai C <saicharan.k.systems@gmail.com>
2026-01-08 20:34:25 +0530

    Create LC_110_balaced_binary_tree.cpp

---
+++ data_structures/tree/LC_balaced_binary_tree.cpp
@@ -0,0 +1,24 @@
+//TC - O(n)
+//SC - O(1)
+//technique - just checking if difference b/w any two subtrees are >1.
+
+class Solution {
+public:
+    int height(TreeNode* root){
+        if(root == NULL) return 0;
+
+        int lh = height(root->left);
+        if(lh == -1) return -1;
+        int rh = height(root->right);
+        if(rh == -1) return -1;
+        if(abs(lh - rh)>1) return -1;
+
+        return max(lh, rh)+ 1;
+    }
+    bool isBalanced(TreeNode* root) {
+        
+        return height(root) != -1;
+
+        
+    }
+};
