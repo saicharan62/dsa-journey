c48f9548ab1a80cdeae1012c7b021fd7ce0d1a3f
Sai C <saicharan.k.systems@gmail.com>
2026-01-09 19:14:23 +0530

    Create LC_101_symmetric_tree.cpp

---
+++ data_structures/tree/LC_101_symmetric_tree.cpp
@@ -0,0 +1,21 @@
+//TC - O(n)
+//SC - O(1)
+//technique - we check if the values on exact opposite nodes are same, to be a symmetric tree.
+
+class Solution {
+public:
+    bool mirrorTree(TreeNode* root1, TreeNode* root2){
+        if(root1 == NULL && root2 == NULL) return true;
+        if(root1 == NULL || root2 == NULL) return false;
+
+        if(root1->val != root2-> val) return false;
+
+        return mirrorTree(root1->left, root2->right) && mirrorTree(root1->right, root2->left);
+
+    }
+    bool isSymmetric(TreeNode* root) {
+        if(root == nullptr) return true;
+
+        return mirrorTree(root->left, root->right);
+    }
+};
