f11d3d9638dfe415e499944bcdd2724f27e346a1
Sai C <saicharan.k.systems@gmail.com>
2026-01-15 21:20:52 +0530

    Create LC_1448_count_good_nodes_in_bt.cpp

---
+++ patterns/dfs/LC_1448_count_good_nodes_in_bt.cpp
@@ -0,0 +1,21 @@
+//TC - O(n)
+//SC - O(h)
+//technique - we keep track of max element so far in the path and swap, if it's < than current element.
+
+class Solution {
+public:
+    int dfs(TreeNode* root, int maxSoFar){
+        if(root == NULL) return 0;
+
+        int good = 0;
+        if(root->val >= maxSoFar){
+            good = 1;
+            maxSoFar = root->val;
+        }
+
+        return good + dfs(root->left, maxSoFar) + dfs(root->right, maxSoFar);
+    }
+    int goodNodes(TreeNode* root) {
+        return dfs(root, root->val);
+    }
+};
