b2ef22383db9461d6973447d8bdf6af23e68776f
Sai C <saicharan.k.systems@gmail.com>
2026-01-21 17:01:16 +0530

    Create LC_14_longest_common_prefix.cpp

---
+++ data_structures/arrays/LC_14_longest_common_prefix.cpp
@@ -0,0 +1,18 @@
+//TC - O(n*m)
+//SC - O(1)
+//technique - we use vertical scanning by checking every char of strings in array parallely.
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+
+         for(int i=0; i<strs[0].length(); i++){
+            for(const string& s:strs){
+                if(i == s.length() || s[i] != strs[0][i]){
+                    return s.substr(0, i);
+                }
+            }
+         }
+         return strs[0];
+    }
+};
