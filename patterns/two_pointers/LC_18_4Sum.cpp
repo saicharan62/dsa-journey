12089543e13347d66b3ab6417f584b79746e705b
Sai C <saicharan.k.systems@gmail.com>
2026-02-24 15:12:27 +0530

    Create LC_18_4Sum.cpp

---
+++ patterns/two_pointers/LC_18_4Sum.cpp
@@ -0,0 +1,44 @@
+// TC - O(n^3)
+// SC - O(n)
+// technique - we first fix two numbers using nested loops and then perform 2Sum logic for remaining two numbers, but avoiding duplicates is important.
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> list;
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+
+        for(int i=0; i<n; i++){
+            if(i>0 && nums[i] == nums[i-1]) continue;
+
+            for(int j=i+1; j<n; j++){
+                if(j>i+1 && nums[j] == nums[j-1]) continue;
+            
+                int l = j+1;
+                int r = n-1;
+                while(l<r){
+                    long long sum = (long long) nums[i]+nums[j]+nums[l]+nums[r];
+
+                    if(sum == target){
+                        list.push_back({nums[i], nums[j], nums[l], nums[r]});
+                        l++;
+                        r--;
+
+                        while(l<r && nums[l] == nums[l-1]) l++;
+                        
+                        while(l<r && nums[r] == nums[r+1]) r--;
+                        
+                    }
+                    else if(sum > target){
+                        r--;
+                    }
+                    else{
+                        l++;
+                    }
+                }
+            }
+        }
+        return list;
+    }
+};
