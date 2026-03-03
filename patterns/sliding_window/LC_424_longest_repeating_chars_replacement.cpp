0963ab9e6c133f94044f0a56019351309ed9f2c6
Sai C <saicharan.k.systems@gmail.com>
2026-03-03 08:31:29 +0530

    Create LC_424_longest_repeating_chars_replacement.cpp

---
+++ patterns/sliding_window/LC_424_longest_repeating_chars_replacement.cpp
@@ -0,0 +1,26 @@
+// TC - O(n)
+// SC - O(1)
+// technique - if the replacements req. is > k, then we shrink the window, or else we can proceed with freq. counting and looking for max. length of sub string with single char.
+
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int l = 0, r = 0;
+        int maxFreq = 0;
+        int maxLen = 0;
+        vector<int> freq(26,0);
+        while(r < s.size()){
+            freq[s[r] - 'A']++;
+
+            maxFreq = max(maxFreq, freq[s[r] - 'A']);
+
+            while((r-l+1) - maxFreq > k){
+                freq[s[l] - 'A']--;
+                l++;
+            }
+            maxLen = max(maxLen, r-l+1);
+            r++;
+        }
+        return maxLen;
+    }
+};
