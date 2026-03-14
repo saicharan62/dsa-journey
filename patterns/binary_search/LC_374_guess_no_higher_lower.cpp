85fe2ea0f39f3c985b4c508a1f77848dfcfff8c5
Sai C <saicharan.k.systems@gmail.com>
2026-03-14 18:48:46 +0530

    Create LC_374_guess_no_higher_lower.cpp

---
+++ patterns/binary_search/LC_374_guess_no_higher_lower.cpp
@@ -0,0 +1,30 @@
+// TC - O(log n)
+// SC - O(1)
+
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int l = 0;
+        int h = n;
+
+        while(l <= h){
+            int m = l + (h-l)/2;
+            int res = guess(m);
+
+            if(res == 0) return m;
+
+            else if(res == 1) l = m+1;
+            else h = m-1;
+        }
+        return -1;
+    }
+};
