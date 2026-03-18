e593bc80364757385180e89c07a2639edee32629
Sai C <saicharan.k.systems@gmail.com>
2026-03-18 13:23:09 +0530

    Update LC_53_maximum_subarray.cpp

--- patterns/greedy/LC_53_maximum_subarray.cpp
+++ patterns/greedy/LC_53_maximum_subarray.cpp
@@ -14,3 +14,23 @@ public:
         return maxS;
     }
 };
+
+// same implementation but for contests and leetcode
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxi = INT_MIN;
+        int sum = 0;
+        for(int i=0; i<nums.size(); i++){
+            sum += nums[i];
+            if(sum > maxi){
+                maxi = sum;
+            }
+            if(sum < 0){
+                sum = 0;
+            }
+        }
+        return maxi;
+    }
+
+};
